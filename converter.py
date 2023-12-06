34a17f458f125b97b2add36807ada7e47255422e
varyg <88599103+varyg1001@users.noreply.github.com>
2023-12-06 11:36:45 +0100

    nC: add remove bumper, other fixes and improvments

--- converter.py
+++ converter.py
@@ -11,6 +11,12 @@ from pysubs2 import SSAFile, SSAEvent, SSAStyle
 parser = argparse.ArgumentParser(description="hidi subtitle converter")
 parser.add_argument("path", type=Path, nargs="*", default=None, help="Input file.")
 parser.add_argument(
+    "-r",
+    "--remove-bumper",
+    action="store_true",
+    help="remove long intro time from time",
+)
+parser.add_argument(
     "-c",
     "--css-file",
     type=Path,
@@ -33,8 +39,9 @@ COLORS = {
     "FFFFFF": "FFFF",
 }
 
-resx: int = 640
-resy: int = 360
+res_x: int = 640
+res_y: int = 360
+
 
 def get_mill(time: str) -> int:
     """
@@ -54,51 +61,57 @@ def get_mill(time: str) -> int:
 for path in args.path:
     formatting = list()
     names = list()
-    sub = SSAFile()
     styles = set()
+    sub = SSAFile()
 
     # styles
     sub.styles["Default"].shadow = 0
     sub.styles["Default"].outline = 1
-    sub.styles["Default"].fontsize = 25
+    sub.styles["Default"].fontsize = 22
     sub.styles["Top"] = sub.styles["Default"].copy()
     sub.styles["Top"].alignment = SSAStyle.alignment.TOP_CENTER
+    sub.styles["Top"].fontsize = 20
 
+    # get all styles
     if styles := re.findall(r"<c.(.*)-C[0-9]+_[1-9]", path.read_text()):
         styles = set(styles)
 
     with open(path) as f:
         lines = f.readlines()
-        for x in styles:
-            if "Subtitle" not in x:
-                sub.styles[x] = sub.styles["Default"].copy()
+    for x in styles:
+        if "Subtitle" not in x:
+            sub.styles[x] = sub.styles["Default"].copy()
 
-        if "Caption" in styles:
-            sub.styles["Caption"].shadow = 0.5
+    if "Caption" in styles:
+        sub.styles["Caption"].shadow = 0.5
+    elif "Song" in styles:
+        sub.styles["Song"].fontsize = 20
 
-        for x, line in enumerate(lines):
-            if line.startswith(tuple(styles)):
-                names.append(line.strip())
-                temp = lines[x + 1].split(" ")
-                data = re.search(r"<c.*>(.+?)</c.*>", lines[x + 2]).group(1)
-                event = SSAEvent(text=data)
+    for x, line in enumerate(lines):
+        if line.startswith(tuple(styles)):
+            names.append(line.strip())
+            temp = lines[x + 1].split(" ")
+            data = re.search(r"<c.*>(.+?)</c.*>", lines[x + 2]).group(1)
+            event = SSAEvent(text=data)
 
-                if line.startswith("Song"):
-                    event.style = "Song"
-                if line.startswith("Caption"):
-                    position = re.search(r"position:(\d+)", lines[x + 1]).group(1) or 0
-                    line = re.search(r"line:(\d+)", lines[x + 1]).group(1) or 0
-                    event.text = f"{{\\an7\\pos({round(resx*float(position) / 100, 2)},{round(resy*float(line) / 100, 2)})}}{event.text}"
-                    event.style = "Caption"
-                else:
-                    if line := re.search(r"line:(\d+)", lines[x + 1]):
-                        line = line.group(1)
-                        if int(line) < 71:
-                            event.style = "Top"
+            if line.startswith("Song"):
+                event.style = "Song"
+            if line.startswith("Caption"):
+                position = re.search(r"position:(\d+)", lines[x + 1]).group(1) or 0
+                line = re.search(r"line:(\d+)", lines[x + 1]).group(1) or 0
+                ry = round(res_y * float(line) / 100, 2)
+                rx = round(res_x * float(position) / 100, 2)
+                event.text = f"{{\\an7\\pos({rx},{ry})}}{event.text}"
+                event.style = "Caption"
+            else:
+                if line := re.search(r"line:(\d+)", lines[x + 1]):
+                    line = line.group(1)
+                    if int(line) < 71:
+                        event.style = "Top"
 
-                sub.append(event)
-                event.start = get_mill(temp[0])
-                event.end = get_mill(temp[2])
+            sub.append(event)
+            event.start = get_mill(temp[0])
+            event.end = get_mill(temp[2])
 
     if args.css_file:
         with open(args.css_file) as f:
@@ -119,12 +132,14 @@ for path in args.path:
                 ]
                 for y in remove:
                     x = x.replace(y, "")
+
                 data = x.strip().replace(", ", "||").split("||")
                 if (temp := data[-1].split("{")) and len(temp) == 2:
                     data[-1] = temp[0]
                     data.append(temp[-1].split(";"))
                 formatting.append(data)
 
+    # filter and add formatting from css file
     for x in formatting:
         add = ""
         font = [y for y in x[-1] if "font-family" in y]
@@ -158,12 +173,14 @@ for path in args.path:
                         size_ = sub.styles[line.style].fontsize
                         line.text = line.text.replace(
                             f"fs{size}",
-                            f"fs{int(size_*float(size))}",
+                            f"fs{int(size_ * float(size))}",
                         )
 
     # new SSAFile for merged lines
     sub_formatted = deepcopy(sub)
     sub_formatted.clear()
+
+    # merge lines
     skip: bool = False
     for s, x in enumerate(sub):
         if names[s][-1] != "1":
@@ -173,6 +190,18 @@ for path in args.path:
                     if "c&HFFFF&" in sub[s - 1].text and "{" not in x.text
                     else ""
                 )
+                if "}" in x.text:
+                    sec = x.text.split("}")
+                    sec_ = sec[0].strip("{\\").split("\\")
+                    sec_ = [
+                        y
+                        for y in sec_
+                        if ("pos" not in y) and (y not in sub[s - 1].text)
+                    ]
+                    if sec_:
+                        x.text = "{" + "\\".join(sec_) + sec[-1]
+                    else:
+                        x.text = sec[-1]
                 sub[s - 1].text += f"\\N{color}{x.text}"
                 s += 1
                 skip = True
@@ -181,9 +210,15 @@ for path in args.path:
         else:
             sub_formatted.append(x)
 
-    path = path.with_suffix(".ass")
-    sub_formatted.info["PlayResX"] = str(resx)
-    sub_formatted.info["PlayResY"] = str(resy)
+    # set infos
+    sub_formatted.info["PlayResX"] = str(res_x)
+    sub_formatted.info["PlayResY"] = str(res_y)
     sub_formatted.info["YCbCr Matrix"] = "TV.709"
 
-    sub_formatted.save(path)
+    if args.remove_bumper:
+        b_time = get_mill("00:00:04.963")
+        for line in sub_formatted:
+            line.start -= b_time
+            line.end -= b_time
+
+    sub_formatted.save(path.with_suffix(".ass"))
